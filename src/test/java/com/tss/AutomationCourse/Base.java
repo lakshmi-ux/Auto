aead1ee14b40ff977a3c50d267bd8408e1d3bc76
Emvigo <Emvigo@EM-025-MLAKSHMI>
2024-06-12 09:01:23 +0530

    latest code

---
+++ src/test/java/com/tss/AutomationCourse/Base.java
@@ -0,0 +1,14 @@
+package com.tss.AutomationCourse;
+
+public class Base {
+	public void initializeBrowser() {
+		
+	}
+
+	public static void main(String[] args) {
+		Base base= new Base();
+		base.initializeBrowser();
+
+	}
+
+}
